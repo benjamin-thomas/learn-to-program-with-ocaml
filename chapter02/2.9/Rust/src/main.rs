08ce4fd644d762153d70b359468a19e94041150d
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-10-06 14:20:23 +0200

    Tweak rust code

--- chapter02/2.9/Rust/src/main.rs
+++ chapter02/2.9/Rust/src/main.rs
@@ -8,8 +8,26 @@ fn digit_of_char(c: char) -> Result<u8, AppError> {
     match c {
         '0'..='9' => Ok((c as u8) - b'0'),
         'A'..='Z' => Ok((c as u8) - b'A' + 10),
+        'a'..='z' => Ok((c as u8) - b'a' + 10),
         _ => Err(AppError::ParseError(c)),
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_digit_of_char() {
+        assert!(matches!(digit_of_char('$'), Err(AppError::ParseError('$'))));
+        assert!(matches!(digit_of_char('0'), Ok(0)));
+        assert!(matches!(digit_of_char('F'), Ok(15)));
+        assert!(matches!(digit_of_char('f'), Ok(15)));
+    }
+}
+
+fn hex_to_digits(digits: &[u8]) -> usize {
+    digits.iter().fold(0, |acc, &x| (x as usize) + acc * 16)
 }
 
 fn run_loop() -> Result<(), AppError> {
@@ -26,12 +44,12 @@ fn run_loop() -> Result<(), AppError> {
                 let input = buf.trim();
                 println!("{}", input);
 
-                let result = input
+                let digits = input
                     .chars()
                     .map(digit_of_char)
-                    .collect::<Result<Vec<u8>, AppError>>()?
-                    .iter()
-                    .fold(0, |acc, &x| (x as usize) + acc * 16);
+                    .collect::<Result<Vec<u8>, AppError>>()?;
+
+                let result = hex_to_digits(&digits);
 
                 println!(" -> {:}\n", result)
             }
@@ -40,9 +58,18 @@ fn run_loop() -> Result<(), AppError> {
     Ok(())
 }
 
+/*
+
+rg --files | entr -c cargo test
+rg --files | entr -rc bash -c 'cargo run < <(echo -e "123\nFF")'
+
+*/
 fn main() {
+    println!("Starting up...");
     match run_loop() {
         Err(x) => eprintln!("Program failed: {:?}", x),
         Ok(()) => {}
     }
+
+    println!("Finished!");
 }
