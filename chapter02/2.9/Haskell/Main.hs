e59ecb93ffbf29212fea1f2bb65e7de82f7e0da5
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-10-06 13:18:35 +0200

    Tweaks

--- chapter02/2.9/Haskell/Main.hs
+++ chapter02/2.9/Haskell/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Monad (unless)
-import Data.Char (isDigit)
+import Data.Char (isAsciiLower, isAsciiUpper, isDigit)
 import Data.List (intercalate)
 import System.Environment.Blank (getArgs)
 import System.IO (isEOF)
@@ -21,9 +21,44 @@ reset = "\x1b[0m"
 toBase10 :: Char -> Maybe Int
 toBase10 c
     | isDigit c = Just $ fromEnum c - fromEnum '0'
-    | c >= 'A' && c <= 'F' = Just $ fromEnum c - fromEnum 'A' + 10
-    | c >= 'a' && c <= 'f' = Just $ fromEnum c - fromEnum 'a' + 10
+    | isAsciiUpper c = Just $ fromEnum c - fromEnum 'A' + 10
+    | isAsciiLower c = Just $ fromEnum c - fromEnum 'a' + 10
     | otherwise = Nothing
+
+{- | Converts a string representation of a number to its base 10 representation. Supports converting numbers from bases 2-36.
+
+>>> convertToBase10 16 "ABC"
+([12,11,10],"")
+
+>>> convertToBase10 16 "EFG"
+([15,14],"G")
+
+>>> convertToBase10 36 "XYZ"
+([35,34,33],"")
+
+>>> convertToBase10 37 "XYZ"
+([],"ZYX")
+
+>>> convertToBase10 2 "0123"
+([1,0],"32")
+
+>>> convertToBase10 1 "0123"
+([],"3210")
+-}
+convertToBase10 :: Int -> String -> ([Int], [Char])
+convertToBase10 base =
+    foldl
+        ( \(good, bad) c ->
+            case toBase10 c of
+                Nothing -> (good, c : bad)
+                Just n ->
+                    if n < 0 || n >= base || base > 36 || base < 2
+                        then
+                            (good, c : bad)
+                        else
+                            (n : good, bad)
+        )
+        ([], [])
 
 {-
 
@@ -35,10 +70,37 @@ Is equivalent to:
 
 I'm using foldr to handle the list which is already in reverse order.
 
- -}
+While converting "0xABC":
+>>> hornerRev 16 [12,11,10]
+2748
+-}
 hornerRev :: Int -> [Int] -> Int
 hornerRev base =
     foldr (\n acc -> n + base * acc) 0
+
+handleLine :: Int -> [Char] -> IO ()
+handleLine base line = do
+    putStrLn $
+        mconcat
+            [ "["
+            , intercalate "," $ map show line
+            , "]"
+            ]
+
+    let (good, bad) = convertToBase10 base line
+
+    case (good, bad) of
+        (vals, []) -> do
+            print (reverse vals) -- just for clarity, for the user
+            putStrLn $ " -> " <> show (hornerRev base vals)
+        (_, chars) ->
+            putStrLn $
+                mconcat
+                    [ red
+                    , " => Bad chars: " <> chars
+                    , reset
+                    ]
+    putStrLn ""
 
 mainLoop :: Int -> IO ()
 mainLoop base = do
@@ -48,38 +110,8 @@ mainLoop base = do
     loop :: IO ()
     loop = do
         line <- getLine
-        putStrLn $
-            mconcat
-                [ "["
-                , intercalate "," $ map show line
-                , "]"
-                ]
-
-        let (good, bad) = convertToBase10 line
-
-        case (good, bad) of
-            (vals, []) -> do
-                print (reverse vals) -- just for clarity, for the user
-                putStrLn $ " -> " <> show (hornerRev base vals)
-            (_, chars) ->
-                putStrLn $
-                    mconcat
-                        [ red
-                        , " => Bad chars: " <> chars
-                        , reset
-                        ]
-        putStrLn ""
+        handleLine base line
         mainLoop base
-      where
-        convertToBase10 :: [Char] -> ([Int], [Char])
-        convertToBase10 =
-            foldl
-                ( \(good, bad) c ->
-                    case toBase10 c of
-                        Just n -> (n : good, bad)
-                        Nothing -> (good, c : bad)
-                )
-                ([], [])
 
 main :: IO ()
 main = do
