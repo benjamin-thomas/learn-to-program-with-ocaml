e0b5de711758a69e27fc2b9330aa2cd3d745da91
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-10-26 22:08:28 +0200

    Finish 2.2 rosace exercise

---
+++ chapter02/2.15/bin/rosace.ml
@@ -0,0 +1,29 @@
+(*
+   dune exec --no-print-directory --display=quiet bin/rosace.exe
+*)
+
+module G = Graphics
+
+let () =
+  G.open_graph " 800x800";
+  let pi = 4. *. atan 1. in
+  let center_x = 400. in
+  let center_y = 400. in
+  let offset = 100. in
+  let circle_size = 100 in
+  let circle n =
+    G.draw_circle
+      (truncate @@ (center_x +. (offset *. cos (pi *. n))))
+      (truncate @@ (center_y +. (offset *. sin (pi *. n))))
+      circle_size
+  in
+  print_endline "rosace";
+  G.draw_circle 400 400 circle_size;
+  circle @@ (1. /. 3.);
+  circle @@ (2. /. 3.);
+  circle @@ (3. /. 3.);
+  circle @@ (4. /. 3.);
+  circle @@ (5. /. 3.);
+  circle @@ (6. /. 3.);
+  input_line stdin |> ignore
+;;
