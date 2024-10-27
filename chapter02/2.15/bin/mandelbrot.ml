e26455d6b31d1d564e7c6807a3ec1950c2187174
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2024-10-27 16:12:47 +0100

    Mandelbrot: add color

--- chapter02/2.15/bin/mandelbrot.ml
+++ chapter02/2.15/bin/mandelbrot.ml
@@ -8,6 +8,21 @@ let height = 800
 
     See the "tic-tac-toe" board at: https://www.wikihow.com/Plot-the-Mandelbrot-Set-By-Hand *)
 let normalize n total = (4. *. float_of_int n /. float_of_int total) -. 2.
+[@@warning "-32"]
+;;
+
+let normalize_scaled n total center scale =
+  (4. *. scale *. (float_of_int n /. float_of_int total))
+  -. (2. *. scale)
+  -. (center *. scale /. 2.)
+;;
+
+(* This isn't quite right *)
+let zoom_to_click x y scale (center_x, center_y) =
+  let scale = scale *. 0.8 in
+  ( scale
+  , (normalize_scaled x width center_x scale, normalize_scaled y height center_y scale) )
+;;
 
 let magnitude x y = (x *. x) +. (y *. y)
 
@@ -29,35 +44,52 @@ let caption str line =
   G.draw_string str
 ;;
 
-let draw iterations =
+let draw iterations scale (center_x, center_y) =
   let board = Array.make_matrix height width 0 in
   for x = 0 to width - 1 do
     for y = 0 to height - 1 do
-      let a = normalize x width in
-      let b = normalize y height in
+      let a = normalize_scaled x width center_x scale in
+      let b = normalize_scaled y height center_y scale in
       let remain = mandelbrot iterations (a, b) in
       let color = truncate @@ (float remain /. float iterations *. float G.white) in
       board.(y).(x) <- color
     done
   done;
   G.draw_image (G.make_image board) 0 0;
-  (* Calling `moveto` ensures the text doesn't get draw off screen since the cursor moves upon each call *)
   G.moveto 10 5;
   G.draw_string @@ sprintf "Iterations: %d" iterations;
-  caption "press '+' to increase iterations" 1;
+  caption "press '+' to increase iterations, '-' to decrease" 1;
   caption "press 'q' to quit" 2;
   ()
 ;;
 
-let rec loop iterations =
-  draw iterations;
+let rec loop iterations scale (center_x, center_y) =
+  draw iterations scale (center_x, center_y);
   let rec listen_keyboard () =
-    let evt = G.wait_next_event [ Key_pressed ] in
-    match evt.key with
-    | '+' -> loop (iterations + 1)
-    | '-' -> loop (iterations - 1)
-    | 'q' -> G.close_graph ()
-    | _ -> listen_keyboard ()
+    let evt = G.wait_next_event [ Key_pressed; Button_down ] in
+    match evt with
+    | { G.button = true; mouse_x = x; mouse_y = y; _ } ->
+      let (scale, (center_x, center_y)) = zoom_to_click x y scale (center_x, center_y) in
+      loop iterations scale (center_x, center_y)
+    | { G.key = '+'; _ } -> loop (iterations + 1) scale (center_x, center_y)
+    | { G.key = '-'; _ } -> loop (iterations - 1) scale (center_x, center_y)
+    | { G.key = '\224'; _ } ->
+      loop iterations (scale *. 0.8) (center_x, center_y) (* Zoom in *)
+    | { G.key = 'y'; _ } ->
+      loop iterations (scale /. 0.8) (center_x, center_y) (* Zoom out *)
+    | { G.key = 'u'; _ } ->
+      loop iterations scale (center_x, center_y -. (scale *. 0.2)) (* up *)
+    | { G.key = '\233'; _ } ->
+      loop iterations scale (center_x, center_y +. (scale *. 0.2)) (* down *)
+    | { G.key = 'a'; _ } ->
+      loop iterations scale (center_x +. (0.2 *. scale), center_y) (* left *)
+    | { G.key = 'i'; _ } ->
+      loop iterations scale (center_x -. (0.2 *. scale), center_y) (*  right *)
+    | { G.key = 'r'; _ } -> loop 15 1.0 (1., 1.)
+    | { G.key = 'q'; _ } -> G.close_graph ()
+    | _ ->
+      printf "key=%d\n%!" @@ int_of_char evt.key;
+      listen_keyboard ()
   in
   listen_keyboard ()
 ;;
@@ -65,5 +97,5 @@ let rec loop iterations =
 let () =
   G.open_graph @@ sprintf " %dx%d" width height;
   G.set_window_title "Mandelbrot";
-  loop 15
+  loop 15 1.0 (1., 1.)
 ;;
